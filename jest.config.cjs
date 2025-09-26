0bcff74a567c3324b82f4496e19bb679f9d22d99
Your Name <you@example.com>
2025-09-26 17:43:28 +0530

    ci: add Codecov upload + coverage config

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,17 +5,22 @@ const createJestConfig = nextJest({ dir: './' });
 
 const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
-  // Add jest-dom + any custom matchers
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  // Pick up both __tests__ and *.test / *.spec files
   testMatch: [
     '<rootDir>/**/__tests__/**/*.(test|spec).[jt]s?(x)',
     '<rootDir>/**/*.(test|spec).[jt]s?(x)'
   ],
   moduleNameMapper: {
-    // If you use import aliases like "@/components/.."
     '^@/(.*)$': '<rootDir>/$1'
-  }
+  },
+  // --- Coverage settings ---
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx,js,jsx}',
+    'components/**/*.{ts,tsx,js,jsx}',
+    'utils/**/*.{ts,tsx,js,jsx}'
+  ],
+  coverageReporters: ['text', 'lcov']
 };
 
 module.exports = createJestConfig(customJestConfig);
